80cb594c570ff4985fb908ffc908eeb55f9f08fd
DubeyRitik <ritikdubeysuraj@gmail.com>
2023-02-24 16:08:05 +0530

    added login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import LoginSignup from "./Components/LoginSignup";
 import Navbar from "./Components/Navbar";
 import QuestionDiv from "./Components/QuestionDiv";
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      
+      <LoginSignup></LoginSignup>
     </div>
   );
 }
