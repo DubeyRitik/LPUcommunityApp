4cb218056c18d621dbc56217ff2512978954b4f6
Harikiran-7 <92435534+Harikiran-7@users.noreply.github.com>
2023-02-24 14:48:11 +0530

    added the croos and anywhere outside the popup

--- src/Components/QuesPopup.jsx
+++ src/Components/QuesPopup.jsx
@@ -1,28 +1,53 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { createPortal } from "react-dom";
 import styles from "../CSS Modules/QuesPopup.module.css";
 import { db } from "../firebase.js";
 
 function QuesPopup(props) {
   const inputRef = useRef(null);
+  const [showPopup, setShowPopup] = useState(true);
+
   async function addQues() {
     props.submit();
     let ques = inputRef.current.value;
     await db.collection("questions").add({ question: ques, answered: false });
+    setShowPopup(false);
   }
 
-  return createPortal(
-    <div className={styles.form}>
-      <div className={styles.wrapper}>
-        <div className={styles.actualForm}>
-          <label>Enter your question: </label>
-          <input type="text" ref={inputRef} />
-          <button onClick={addQues}>Submit</button>
-        </div>
-      </div>
-    </div>,
-    document.body
-  );
+  function closePopup() {
+    setShowPopup(false);
+  }
+
+  // Close the popup when the user clicks outside of it
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (inputRef.current && !inputRef.current.contains(event.target)) {
+        setShowPopup(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return showPopup
+    ? createPortal(
+        <div className={styles.form}>
+          <div className={styles.wrapper}>
+            <div className={styles.actualForm}>
+              <button className={styles.closeButton} onClick={closePopup}>
+                X
+              </button>
+              <label>Enter your question: </label>
+              <input type="text" ref={inputRef} />
+              <button onClick={addQues}>Submit</button>
+            </div>
+          </div>
+        </div>,
+        document.body
+      )
+    : null;
 }
 
 export default QuesPopup;
