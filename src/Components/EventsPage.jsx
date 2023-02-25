2dae3d994358526a8cd0f87565eacc797184e7ee
aakashaman <aakashaman9931@gmail.com>
2023-02-25 10:23:06 +0530

    css

--- src/Components/EventsPage.jsx
+++ src/Components/EventsPage.jsx
@@ -1,3 +1,70 @@
+import styles from '../CSS Modules/StudyMaterial.module.css'
+
 export default function EventsPage() {
-  return <div></div>;
+  return <div>
+  <h2>Events</h2>
+<div className={styles.cardset} >
+  
+ <div className={styles.card}>
+  
+ <iframe src="https://www.youtube.com/embed/6nWMMSe5v7g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<div class="card-body">
+  <h5 class="card-title">Placement Day </h5>
+  <p class="card-text">Batch of 2023</p>
+
+</div>
+</div>
+
+<div className={styles.card}>
+<iframe  src="https://www.youtube.com/embed/fbQX1PtHrj8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<div class="card-body">
+  <h5 class="card-title">Life At LPU</h5>
+  <p class="card-text">After Lockdown</p>
+  {/* <a href="https://www.youtube.com/playlist?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA" class="btn btn-primary">Go to Playlist</a> */}
+</div>
+</div>
+
+<div className={styles.card}>
+<iframe  src="https://www.youtube.com/embed/DVBPPrZBqXw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<div class="card-body">
+  <h5 class="card-title">EDM Nights</h5>
+  <p class="card-text">At Unipolis</p>
+  {/* <a href="https://www.youtube.com/playlist?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ" class="btn btn-primary">Go to Playlist</a> */}
+</div>
+
+</div>
+
+<div className={styles.card}>
+<iframe  src="https://www.youtube.com/embed/DVBPPrZBqXw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<div class="card-body">
+  <h5 class="card-title">EDM Nights</h5>
+  <p class="card-text">At Unipolis</p>
+  {/* <a href="https://www.youtube.com/playlist?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ" class="btn btn-primary">Go to Playlist</a> */}
+</div>
+
+
+</div>
+<div className={styles.card}>
+<iframe  src="https://www.youtube.com/embed/5uqo-vFRVo8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<div class="card-body">
+  <h5 class="card-title">Garba</h5>
+  <p class="card-text">At SDMA</p>
+  {/* <a href="https://www.youtube.com/playlist?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ" class="btn btn-primary">Go to Playlist</a> */}
+</div>
+
+
+</div>
+
+<div className={styles.card}>
+<iframe  src="https://www.youtube.com/embed/DVBPPrZBqXw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<div class="card-body">
+  <h5 class="card-title">EDM Nights</h5>
+  <p class="card-text">At Unipolis</p>
+  {/* <a href="https://www.youtube.com/playlist?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ" class="btn btn-primary">Go to Playlist</a> */}
+</div>
+
+
+</div>
+</div>
+</div>
 }
--- src/Components/StudyMaterial.jsx
+++ src/Components/StudyMaterial.jsx
@@ -1,6 +1,6 @@
 
 import styles from '../CSS Modules/StudyMaterial.module.css'
-import { Card, Row, Col, Container } from "react-bootstrap";
+
 
 export default function StudyMaterial() {
 
@@ -13,27 +13,27 @@ export default function StudyMaterial() {
     
    <iframe  src="https://www.youtube.com/embed/EAR7De6Goz4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   <div class="card-body">
-    <h5 class="card-title">STRIVER C++ BASICS</h5>
+    <h5 class="card-title">STRIVER C++ </h5>
     <p class="card-text">Learn basic of C++ </p>
-    <a href="https://www.youtube.com/playlist?list=PLgUwDviBIf0oF6QL8m22w1hIDC1vJ_BHz" class="btn btn-primary">click for playlist</a>
+    <a href="https://www.youtube.com/playlist?list=PLgUwDviBIf0oF6QL8m22w1hIDC1vJ_BHz" class="btn btn-primary">Go to playlist</a>
   </div>
 </div>
 
 <div className={styles.card}>
 <iframe  src="https://www.youtube.com/embed/WQoB2z67hvY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">DSA Course</h5>
+    <p class="card-text"> DSA by Love Babbar</p>
+    <a href="https://www.youtube.com/playlist?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA" class="btn btn-primary">Go to Playlist</a>
   </div>
 </div>
 
 <div className={styles.card}>
-  <iframe src="https://www.youtube.com/embed/pWahNIMRxR0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe src="https://www.youtube.com/embed/rZ41y93P2Qo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">Java Course</h5>
+    <p class="card-text">By Kunal Kushwaha</p>
+    <a href="https://www.youtube.com/playlist?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ" class="btn btn-primary">Go to Playlist</a>
   </div>
 </div>
   </div>;
